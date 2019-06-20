5de530090326000166b1f12d2bcabe9e2f49be0c
ramonahohl <ramonahohl@outlook.com>
2019-06-20 16:01:36 +0200

    changed output frequency

--- adaptor/include/data.hpp
+++ adaptor/include/data.hpp
@@ -6,10 +6,13 @@ public:
     bool getEndTimestep() const;
     void setCurrentTimestep(int value);
     void setEndTimeStep(int value);
+    void setNewTimestep(bool nt);
+    bool getNewTimestep() const;
 
 private:
     int currentTimestep = 0;
     int endTimestep= -1;
+    bool newTimstep = true;
 };
 
 inline int MyData::getCurrentTimestep() const
@@ -29,4 +32,14 @@ inline void MyData::setCurrentTimestep(int value)
 inline void MyData::setEndTimeStep(int value)
 {
     endTimestep = value;
+}
+
+inline bool MyData::getNewTimestep() const
+{
+    return newTimstep;
+}
+
+inline void MyData::setNewTimestep(bool nt)
+{
+    newTimstep = nt;
 }

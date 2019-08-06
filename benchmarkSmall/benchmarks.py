00a043a67338adb2061328723b481f2312a2a9d2
ramonahohl <ramonahohl@outlook.com>
2019-08-06 13:58:51 +0200

    small change in script

--- benchmarkSmall/benchmarks.py
+++ benchmarkSmall/benchmarks.py
@@ -1,3 +1,4 @@
+
 #! /usr/bin/env python3
 # encoding: utf-8
 
@@ -22,9 +23,6 @@ def todays_scratch_directory():
     return scratch
 
 
-def write_xml
-
-
 ncores = "8"
 ns = ["128", "512", "1024", "2048"]
 cases = ["direct_no_io", "direct_io", "cat_pvti", "cat_png"]
@@ -39,10 +37,10 @@ subprocess.check_call(["rm", "-rf", "b_*"], cwd =basedir)
 for n in ns:
     for i in range(0,len(cases)):
         c = cases[i]
-        xmlname = basedir+"benchmarkSmall/benchmark_"+xmlcases[i]+"_"+ns+".xml"
+        xmlname = basedir+"benchmarkSmall/benchmark_"+xmlcases[i]+"_"+n+".xml"
         newfolder = "b_"+c+"_"+n
         subprocess.check_call(["mkdir", newfolder], cwd =basedir)
         newfolder = basedir+"/"+newfolder
         subprocess.check_call(["cmake", ".."], cwd = newfolder)
         subprocess.check_call(["make"], cwd = newfolder)
-        subprocess.check_call(["OMP_NUM_THREADS=",ncores,"bsub", "-n","-W", "4:00", ncores, alsvinn, xmlname], cwd = newfolder)
+        subprocess.check_call(["OMP_NUM_THREADS=",ncores,"bsub", "-n", ncores,"-W", "4:00", alsvinn, xmlname], cwd = newfolder)

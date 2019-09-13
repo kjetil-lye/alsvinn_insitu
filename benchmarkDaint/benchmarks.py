d5317a5186052a1f8a2533c97d7d0b1518337871
Ramona Hohl <rhohl@daint104.login.cscs.ch>
2019-09-13 16:09:58 +0200

    moving

--- benchmarkDaint/benchmarks.py
+++ benchmarkDaint/benchmarks.py
@@ -4,31 +4,33 @@ import datetime
 import os
 import subprocess
 
+
+dryrun = True
 wtime ="24"
-nnodes = "1"
+nnodes = "100"
 dim ="3"
+
 ns = ["64","128","256"]
-#cases = ["cat_pvti"]
-cases = ["direct_no_io", "direct_io", "cat_pvti", "cat_png"]
-#cases = [ "cat_pvti", "cat_png"]
-#xmlcases=[ "catalyst", "catalyst_pngs"]
-xmlcases = ["no_io", "io", "catalyst", "catalyst_pngs"]
+cases = ["direct_no_io", "cat_pvti", "cat_png"]
+xmlcases = ["no_io", "catalyst", "catalyst_pngs"]
 
 basedir = "/scratch/snx3000/rhohl/alsvinn_insitu/"
 #basedir = "/users/rhohl/alsvinn_insitu/" #/cluster/home/hohlr/alsvinn_insitu/" #/home/ramona/MasterthesisLOCAL/coding/alsvinn_insitu" #"
 alsvinn = "/users/rhohl/alsvinn/build/alsuqcli/alsuqcli" # "/cluster/home/hohlr/alsvinn/build/alsuqcli/alsuqcli"
 
-#subprocess.check_call(["rm", "-rf", "build_*"], cwd =basedir)
 
 for n in ns:
     for i in range(0,len(cases)):
         c = cases[i] 
         xmlname = basedir+"benchmarkDaint/inputfiles_"+dim+"d_1sample/benchmark_"+xmlcases[i]+"_"+n+".xml"
+        if(int(nnodes)>1):
+        	xmlname = basedir+"benchmarkDaint/inputfiles_"+dim+"d_"+nnodes+"samples/benchmark_"+xmlcases[i]+"_"+n+".xml"
 	jobname = xmlcases[i]+"_"+n+"_"+dim+"d_"+nnodes
 	newfolder = "build_"+c+"_"+n+"_"+dim+ "d_"+nnodes
-	subprocess.check_call(["mkdir", newfolder], cwd =basedir)
+	if(not dryrun):
+		subprocess.check_call(["mkdir", newfolder], cwd =basedir)
 	newfolder = basedir+newfolder
- 	if(c == "cat_png" or c== "cat_pvti"):       
+ 	if(not dryrun and (c == "cat_png" or c== "cat_pvti")):       
 		subprocess.check_call(["cmake", "-DOPENGL_opengl_LIBRARY=/opt/cray/nvidia/default/lib64/libOpenGL.so", "-DTBB_INCLUDE_DIR=/opt/intel/compilers_and_libraries/linux/tbb/include", "-DTBB_LIBRARY_RELEASE=/opt/intel/compilers_and_libraries/linux/tbb/lib/intel64/gcc4.7/libtbb.so", ".."], cwd = newfolder)
         	subprocess.check_call(["make"], cwd = newfolder)
 	print ' xml file :'
@@ -37,9 +39,13 @@ for n in ns:
 	print newfolder
 	print 'alsvinn:'
 	print alsvinn
-	subprocess.check_call(["cp", xmlname, "."], cwd = newfolder)
-	# subprocess.check_call(["OMP_NUM_THREADS=",ncores,"bsub", "-n",ncores, "-W", "4:00", alsvinn, xmlname], cwd = newfolder)
-#	subprocess.check_call(["bsub", "-n",nnodes, "-W", "4:00", alsvinn, xmlname], cwd = newfolder)
+	if(not dryrun):
+		subprocess.check_call(["cp", xmlname, "."], cwd = newfolder)
+		subprocess.check_call(["cp",  basedir+"benchmarkDaint/inputfiles_"+dim+"d_"+nnodes+"samples/*.py" , "."], cwd = newfolder)
         totcommand = alsvinn+ " " + xmlname 
+        if(int(nnodes)>1):
+		totcommand = alsvinn+ " --multi-sample "+nnodes+" " + xmlname 
         subprocess.check_call(["python3", "submit_command_on_daint.py", "-w", newfolder,  "--command" ,totcommand, "--name", jobname,"--wait_time", wtime,"--nodes", nnodes,  "--dry_run"])
-        subprocess.check_call(["python3", "submit_command_on_daint.py", "-w", newfolder,  "--command" ,totcommand, "--name", jobname,"--wait_time", wtime,"--nodes", nnodes]) #, "--dry_run"])
+       
+	if(not dryrun):
+		subprocess.check_call(["python3", "submit_command_on_daint.py", "-w", newfolder,  "--command" ,totcommand, "--name", jobname,"--wait_time", wtime,"--nodes", nnodes]) #, "--dry_run"])

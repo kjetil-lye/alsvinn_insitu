742922086ba1145e64eea250a8a5d0de51a51590
ramonahohl <ramonahohl@outlook.com>
2019-10-10 16:49:32 +0200

    restructuring compiles on master

--- adaptor/src/dll_adaptor.cpp
+++ adaptor/src/dll_adaptor.cpp
@@ -1,7 +1,7 @@
 #include "dll_adaptor.hpp"
 #include "parameters.hpp"
 #include "data.hpp"
-#include "historgram.hpp"
+#include "histogram.hpp"
 #include "isosurfaceVtkPipeline.hpp"
 #include <mpi.h>
 #include <fstream>
@@ -206,6 +206,153 @@ DLL_ADAPTOR_EXPORT void CatalystCoProcess(void* data, void* parameters, double t
 
       }//endif(ADAPTOR_HISTORGAM)
 }
+
+
+void CatalystCoProcesHistogram(void* data, void* parameters, double time,
+                                          const char* variable_name,  double* variable_data, int nx, int ny, int nz,
+                                          int ngx, int ngy, int ngz, double ax, double ay, double az, double bx,
+                                          double by, double bz, int gpu_number )
+
+{
+                 auto my_parameters = static_cast<MyParameters*>(parameters);
+
+                  int mpi_rank;
+                  MPI_Comm_rank(my_parameters->getMPIComm(), &mpi_rank);
+                  int mpi_size;
+                  MPI_Comm_size(my_parameters->getMPIComm(), &mpi_size);
+                  //check if we can run all in parallel:
+                  int nsamples = std::stoi(my_parameters->getParameter("samples"));
+                  if(mpi_size < nsamples) {
+                          std::cerr<< "warning: not enough mpi nodes,  reduce sample size to number of mpi nodes: "<< mpi_size<<std::endl;
+                  }
+
+                  // make nsamples per group, to use for normalzation:
+                  if(nsamples%mpi_size!=0) {
+                          std::cerr<< "warning: nsmaples not divisible by mpi_size : "<< nsamples/mpi_size<<std::endl;
+                  }
+
+                  int norm_samples = nsamples;
+                  const size_t ndata = (ngx*2+nx)*(ny+2*ngy)*(nz+2*ngz);
+                  double avrg_data[ndata];
+                  double avrg_sqr_data[ndata];
+
+
+                  //historgram calculcation for specifc points only;
+                  const int nii = std::stoi(my_parameters->getParameter("hist_npoints"));
+                  const bool twoPoint = std::stoi(my_parameters->getParameter("hist_2points"));
+                  double px[nii];
+                  double py[nii];
+                  double pz[nii];
+                  getPoints(px,py,pz, nii);
+                  double *pnt_values;
+                  const int pnt_values_size = nii*nsamples;
+
+
+                          if(mpi_rank ==0)
+                          {
+                                  pnt_values = (double*)malloc(sizeof(double) * (pnt_values_size));
+                          }
+
+                          //collect data points from all frames to store all frame values for specifc points
+                          for ( int i =0; i<nii; i++)
+                          {
+                                  int idx = getRankIndex(nx, ny, nz,  ngx, ngy, ngz, px[i], py[i], pz[i]);
+
+                //                  std::cout<< "rank, pt " << mpi_rank << " "<< idx<<" " <<*(variable_data+idx)<<std::endl;
+                                  MPI_Gather(variable_data+idx, 1,  MPI_DOUBLE,  pnt_values+(i*nsamples), 1, MPI_DOUBLE, 0,  my_parameters->getMPIComm());
+                          }
+
+
+
+                  MPI_Reduce(variable_data, avrg_data, ndata, MPI_DOUBLE, MPI_SUM, 0, my_parameters->getMPIComm());
+
+                  double sqr_variable_data[ndata];
+                  //get squared sum to use for variance, reuse variable_data to save space
+                  for (int i = 0; i< ndata; ++i) {
+                          sqr_variable_data[i] = variable_data[i]*variable_data[i];
+                  }
+
+
+                  MPI_Reduce(&sqr_variable_data, avrg_sqr_data, ndata, MPI_DOUBLE, MPI_SUM, 0, my_parameters->getMPIComm());
+
+
+                  if(mpi_rank == 0)
+                  {
+
+                              //collect data points from all frames to store all frame values for specifc points
+                            const std::string path = my_parameters->getParameter("hist_folder");
+                           //historgram calculcation for specifc points only;
+                            const int nbins = std::stoi(my_parameters->getParameter("hist_nbins"));
+
+                                  for ( int i =0; i<nii; i++)
+                                   {
+                                        std::string pntname = std::to_string( px[i]).substr(0,4)+"x"+ std::to_string( py[i]).substr(0,4)+"y"+ std::to_string( pz[i]).substr(0,4)+"z_"+std::to_string(time).substr(0,4);
+                                         auto minmax = std::minmax_element(pnt_values+(i*nsamples),pnt_values+(i+1)*nsamples );
+                                      //    make_histogram( variable_name, idxOfInterest[i],  pnt_values+i*nsamples, nsamples, *(minmax.first),  *(minmax.second), nbins,  bins,  hist );
+                                        if(!twoPoint)
+                                        {
+                                            write_histogram(  variable_name, pntname, nsamples, pnt_values+(i*nsamples), *(minmax.first),  *(minmax.second),  nbins, path);
+                                          }
+                                        else if(twoPoint && i <nii-1)
+                                          {
+                                            pntname += "_"+ std::to_string( px[i+1]).substr(0,4)+"x"+ std::to_string( py[i+1]).substr(0,4)+"y"+ std::to_string( pz[i+1]).substr(0,4)+"z_"+std::to_string(time).substr(0,4);
+                                            auto minmax2  = std::minmax_element(pnt_values+(i+1)*nsamples, pnt_values+(i+2)*nsamples);
+                                            write_2pt_histogram( variable_name, nsamples,pntname, pnt_values+(i*nsamples),*(minmax.first),  *(minmax.second), pnt_values+((i+1)*nsamples), *(minmax2.first),  *(minmax2.second),  nbins, path);
+
+                                          }
+
+                                  }
+
+
+                                  int extend[6]  = {0,nx-1,0,ny-1,0,nz-1};//{ngx, nx+ngx, ngy, ny+ngy, ngz, 0}; //nz+ngz };
+
+                                  if (VTKGrid == NULL)
+                                  {
+                                          VTKGrid = vtkImageData::New();
+                                          VTKGrid->SetExtent(extend); //ngx, ngx+nx, ngy, ngy+ny, ngz, ngz+nz);
+                                  }
+
+                                  dataDescription->GetInputDescriptionByName("input")->SetGrid(VTKGrid);
+                                  // For structured grids we need to specify the global data extents
+
+                                  // Create a field associated with points
+                                  vtkDoubleArray* field_array_mean = vtkDoubleArray::New();
+                                  vtkDoubleArray* field_array_var = vtkDoubleArray::New();
+                                  int ntuples = nx*ny*nz;
+                                  field_array_mean->SetNumberOfComponents(1);
+                                  field_array_mean->SetNumberOfTuples(ntuples);
+                                  field_array_var->SetNumberOfComponents(1);
+                                  field_array_var->SetNumberOfTuples(ntuples);
+                                  field_array_mean->SetName( (std::string(variable_name)+"_mean").c_str());
+                                  field_array_var->SetName( (std::string(variable_name)+"_var").c_str() );
+                                  int index = 0;
+                                  int idx = 0;
+
+                                  // ignoring ghost cells (ngy is number of ghost cells in z direction)
+                                  for (int z = ngz; z < nz + ngz; ++z) {
+                                          // ignoring ghost cells (ngy is number of ghost cells in y direction)
+                                          for (int y = ngy; y < ny + ngy; ++y) {
+                                                  // ignoring ghost cells (ngx is number of ghost cells in x direction)
+                                                  for (int x = ngx; x < nx + ngx; ++x) {
+                                                          index = z * (nx + 2 * ngx) * (ny + 2 * ngy) + y * (nx + 2 * ngx) + x;
+                                                          double tmp = avrg_data[index]/double(norm_samples);
+                                                          field_array_mean->SetValue(idx, tmp);
+                                                          //  tmp *=(avrg_data[index]/double(nsamples));
+                                                          tmp =  avrg_sqr_data[index]/double(norm_samples) -tmp*tmp;
+                                                          field_array_var->SetValue(idx, tmp);
+                                                          idx += 1;
+                                                  }
+                                          }
+
+                                  }
+
+                                  VTKGrid->GetPointData()->AddArray(field_array_mean);
+                                  VTKGrid->GetPointData()->AddArray(field_array_var);
+                                  field_array_mean->Delete();
+                                  field_array_var->Delete();
+                          free(pnt_values);
+                  }//end rank 0
+    } //CatalystCoProcesHistogram
 
 
 
